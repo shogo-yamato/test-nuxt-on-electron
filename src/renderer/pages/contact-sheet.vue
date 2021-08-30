983f4a14a3c39fc8faaca46c2d930d2ef745ede3
shogo-yamato <shogo.yamato@arsaga.jp>
2021-08-30 20:37:11 +0900

    feat(scss): separated scss from SFC

--- src/renderer/pages/contact-sheet.vue
+++ src/renderer/pages/contact-sheet.vue
@@ -32,23 +32,3 @@ export default Vue.extend({
   },
 })
 </script>
-
-<style lang="scss" scoped>
-@import 'assets/scss/app';
-
-.contact-sheet-container {
-  display: grid;
-  grid-template-rows: repeat(7, 1fr);
-  grid-template-columns: repeat(5, 1fr);
-  gap: 16px;
-  width: 100%;
-  min-height: 100vh;
-  background-color: $white;
-
-  > .image {
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
-  }
-}
-</style>
